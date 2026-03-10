3abd76cd06b68f7c2baca983274b6fa067e46932
Ridhima <ridhima.1772@gmail.com>
2026-03-11 05:06:34 +0530

    fix render port

--- app.js
+++ app.js
@@ -121,7 +121,7 @@ app.use((err,req,res,next)=>{
 
 
 /* ================= SERVER ================= */
-
-app.listen(8080,()=>{
+const port = process.env.PORT || 8080;
+app.listen(port,()=>{
     console.log("server is listening to port 8080");
 });
