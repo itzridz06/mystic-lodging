d05e089717a26ce894aabaa50cbfbeb5834aa68b
Ridhima <ridhima.1772@gmail.com>
2026-03-11 17:15:41 +0530

    fixed joi category validation

--- schema.js
+++ schema.js
@@ -1,4 +1,4 @@
-const Joi = require('joi');
+const Joi = require("joi");
 
 module.exports.listingSchema = Joi.object({
   listing: Joi.object({
@@ -8,16 +8,19 @@ module.exports.listingSchema = Joi.object({
     country: Joi.string().required(),
     price: Joi.number().required().min(0),
 
-    image: Joi.object({
-      url: Joi.string().allow("").optional()
-    }).optional(),
+    category: Joi.string()
+      .valid("rooms", "mountains", "beach", "camping", "city")
+      .required(),
 
+    image: Joi.object({
+      url: Joi.string().allow("").optional(),
+    }).optional(),
   }).required(),
 });
 
 module.exports.reviewSchema = Joi.object({
-   review: Joi.object({
+  review: Joi.object({
     rating: Joi.number().required().min(1).max(5),
     comment: Joi.string().required(),
-   }).required(),
+  }).required(),
 });
